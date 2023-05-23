30b68f8624578367d8691edb2e031bd8e3eecca9
Diego Ocaranza <diego.ocaranza@outlook.com>
2023-05-23 11:27:03 -0400

    Carga de archivos

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let uwindow = document.querySelector('.users-window');
+
+function show_hide() {
+  if(uwindow.style.display=="none"){
+    uwindow.style.display="block";
+  }else{
+    uwindow.style.display="none";
+  }
+}
